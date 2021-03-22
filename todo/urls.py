585196da73e90224874246fa2d0b0fc146ebb481
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-03-22 10:56:39 +0000

    Create de detail views and template

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from todo.views import todo_list
+from todo.views import todo_list, todo_detail
 
 app_name = "todos"
 
 
 urlpatterns = [
-    path('', todo_list)
+    path('', todo_list),
+    path('<id>/', todo_detail),
 ]
--- todo/views.py
+++ todo/views.py
@@ -6,3 +6,7 @@ from todo.models import Todo
 def todo_list(request):
     todos = Todo.objects.all()
     return render(request, "todo/todo_list.html", {"todo_list": todos})
+
+def todo_detail(request, id):
+    todo = Todo.objects.get(id=id)
+    return render(request, 'todo/todo_detail.html', {'todo': todo})
