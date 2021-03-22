069fac37627e1474934a8e05d74196ea3d0482fa
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-03-22 09:51:33 +0000

    create models and superuser

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    name = models.CharField(max_length=100)
+    due_date = models.DateField()
+    
+    def __str__(self) -> str:
+        return self.name
